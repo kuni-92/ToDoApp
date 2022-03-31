479d601db5882c5d500a5aecbafe73497b1c06ae
kunikuni03 <49485046+kunikuni03@users.noreply.github.com>
2022-03-31 21:33:31 +0900

    initial project

---
+++ ToDoApp/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  ToDoApp
+//  
+//  Created by kunikuni03 on 2022/03/31
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//  
+//  Created by kunikuni03 on 2022/03/31
+//  
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
