7550932e2262bf90f71f3ef1ec050a25d65b0ca3
kunikuni03 <49485046+kunikuni03@users.noreply.github.com>
2022-04-07 11:40:09 +0900

    Add to SampleToDoList

--- ToDoApp/models/ToDoModel.swift
+++ ToDoApp/models/ToDoModel.swift
@@ -19,6 +19,13 @@ extension ToDoModel {
             ToDoModel(title: "Insert Title1"),
             ToDoModel(title: "Insert Title2"),
             ToDoModel(title: "Insert Title3"),
+            ToDoModel(title: "Insert Title4"),
+            ToDoModel(title: "Insert Title5"),
+            ToDoModel(title: "Insert Title6"),
+            ToDoModel(title: "Insert Title7"),
+            ToDoModel(title: "Insert Title8"),
+            ToDoModel(title: "Insert Title9"),
+            ToDoModel(title: "Insert Title10"),
         ]
     }
 }
